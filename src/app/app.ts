68aad6dd282f637e0e0e2fb87bb909bae5aa3c2d
May Guo <xfirecloak@gmail.com>
2026-07-27 12:21:34 -0500

    Started adding routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
-  template: `<router-outlet>
-
-     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <app-home />
-      </section>
-    </main>
-
-  </router-outlet>`,
+  imports: [RouterLink,RouterOutlet, Home],
+  template: `    
+  <nav>
+      <a routerLink="/">Home</a>
+      |
+      <a href="/about">About</a> | 
+      <a href="/cosplay">Cosplay</a>
+      |
+      <a href="/tutorials">Tutorials</a>      |
+      <a href="/contact">Contact</a> |
+      <a href="/links">Links</a>|
+      <a href="/reviews">Reviews</a>
+    </nav>
+    <router-outlet />
+    `,
   styleUrl: './app.scss'
 })
 export class App {
